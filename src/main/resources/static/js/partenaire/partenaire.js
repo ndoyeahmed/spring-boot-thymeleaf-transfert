e078caaf9e0b277231a8b2dd74f074c4382fd53b
Mouhamed NDOYE <ndoyeahmed2602@gmail.com>
2019-12-02 10:39:00 +0000

    add and load list and notification ok

--- src/main/resources/static/js/partenaire/partenaire.js
+++ src/main/resources/static/js/partenaire/partenaire.js
@@ -29,14 +29,9 @@ function addPartner() {
         cache: false,
         timeout: 600000,
         success: function (data) {
-
-            let json = "<h4>Ajax Response</h4><pre>"
-                + JSON.stringify(data, null, 4) + "</pre>";
-            $('#feedback').html(json);
-
-            console.log("SUCCESS : ", data);
-            // $("#btn-search").prop("disabled", false);
-
+            setTextToAttrib(null, true);
+            toastSuccess('Opération effectuée avec succès', 'Success');
+            listPartner();
         },
         error: function (e) {
 
@@ -70,6 +65,7 @@ function listPartner() {
                 "                            </thead>\n" +
                 "<tbody>";
             for (const p of data.data) {
+                console.log(p);
                 json = json +
                     "<tr>" +
                     "<td>" + p.ninea + "</td>" +
@@ -77,6 +73,10 @@ function listPartner() {
                     "<td>" + p.tel + "</td>" +
                     "<td>" + p.mail + "</td>" +
                     "<td>" + p.adresse + "</td>" +
+                    "<td>" +
+                    "<button class='btn btn-primary' data-target='#partenaireModal' data-toggle='modal'" +
+                    " onclick='loadPartner(" + p + ")' id='update-partner'>Modifier</button>" +
+                    "</td>" +
                     "</tr>"
             }
 
@@ -89,14 +89,61 @@ function listPartner() {
 
         },
         error: function (e) {
-
-            /* let json = "<h4>Ajax Response</h4><pre>"
-                 + e.responseText + "</pre>";
-             $('#feedback').html(json);
-
-             console.log("ERROR : ", e);*/
-            // $("#btn-search").prop("disabled", false);
-
+            console.log(e);
         }
     });
+
+}
+
+function loadPartner(partner) {
+    console.log(partner);
+    if (partner) {
+        setTextToAttrib(partner, false)
+    } else console.log('partner is not define')
+}
+
+function setTextToAttrib(val, clear) {
+    if (clear) {
+        $("#nom").val('');
+        $("#email").val('');
+        $("#ninea").val('');
+        $("#tel").val('');
+        $("#adresse").val('');
+    } else {
+        $("#nom").val(val.nom);
+        $("#email").val(val.mail);
+        $("#ninea").val(val.ninea);
+        $("#tel").val(val.tel);
+        $("#adresse").val(val.adresse);
+    }
+}
+
+function toastSuccess(message, title) {
+    $.toast({
+        text: message,
+        title: title,
+        showHideTransition: 'slide',  // It can be plain, fade or slide
+        bgColor: 'green',              // Background color for toast
+        textColor: '#fff',            // text color
+        allowToastClose: false,       // Show the close button or not
+        hideAfter: 5000,              // `false` to make it sticky or time in milliseconds to hide after
+        stack: 5,                     // `false` to show one stack at a time count showing the number of toasts that can be shown at once
+        textAlign: 'left',            // Alignment of text i.e. left, right, center
+        position: 'top-right'       // bottom-left or bottom-right or bottom-center or top-left or top-right or top-center or mid-center or an object representing the left, right, top, bottom values to position the toast on page
+    })
+}
+
+function toastError(message, title) {
+    $.toast({
+        text: message,
+        title: title,
+        showHideTransition: 'slide',  // It can be plain, fade or slide
+        bgColor: 'red',              // Background color for toast
+        textColor: '#fff',            // text color
+        allowToastClose: false,       // Show the close button or not
+        hideAfter: 5000,              // `false` to make it sticky or time in milliseconds to hide after
+        stack: 5,                     // `false` to show one stack at a time count showing the number of toasts that can be shown at once
+        textAlign: 'left',            // Alignment of text i.e. left, right, center
+        position: 'top-right'       // bottom-left or bottom-right or bottom-center or top-left or top-right or top-center or mid-center or an object representing the left, right, top, bottom values to position the toast on page
+    })
 }
